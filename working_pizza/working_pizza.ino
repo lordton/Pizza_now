0dadccc3b16bc9cc3bf970766f7f742764dbd05a
lordton <lord-ton@mail.ru>
2015-12-15 18:41:17 +0300

    v0.2 - button test

--- working_pizza/working_pizza.ino
+++ working_pizza/working_pizza.ino
@@ -1,8 +1,16 @@
+const int buttonPin = 7;     // the number of the pushbutton pin
+const int ledPin =  13;      // the number of the LED pin
+int buttonState = 0;
 int smssend=1;  
 
 #include <SoftwareSerial.h>
 SoftwareSerial mySerial(2, 3); // RX, TX
 void setup() {
+    // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+  
   
   Serial.begin(19200);  //Скорость порта для связи Arduino с компьютером
   Serial.println("Goodnight moon!");
@@ -13,6 +21,20 @@ void setup() {
 }
 
 void loop() {
+  
+    // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+  }
+  
   
   if (mySerial.available())
     Serial.write(mySerial.read());
